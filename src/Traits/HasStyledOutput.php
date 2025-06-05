79200e271528c63b9244a720d4491dc4070ae437
Anton Vix <Gordan.ei@protonmail.ch>
2025-06-05 22:31:59 +0400

    HasStyledOutput trait

---
+++ src/Traits/HasStyledOutput.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vix\Syntra\Traits;
+
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+trait HasStyledOutput
+{
+    protected SymfonyStyle $output;
+
+    /**
+     * @param array<int, string> $headers
+     * @param array<int, array<int, string>> $rows
+     */
+    protected function table(array $headers, array $rows): void
+    {
+        $table = new Table($this->output);
+
+        $table
+            ->setHeaders($headers)
+            ->setRows($rows)
+            ->render();
+    }
+}
