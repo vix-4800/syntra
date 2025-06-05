e7e99631492cfb7e474b701b2f17a13bc0222d5b
Anton Vix <Gordan.ei@protonmail.ch>
2025-06-05 22:29:44 +0400

    custom application class

---
+++ src/Application.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vix\Syntra;
+
+use Symfony\Component\Console\Application as SymfonyApplication;
+
+class Application extends SymfonyApplication
+{
+    //
+}
