dd95ceee91929b7c562869a11674769dbf23e1ae
Anton Vix <Gordan.ei@protonmail.ch>
2025-07-01 20:22:04 +0400

    name option removed

--- src/Commands/General/GenerateCommandCommand.php
+++ src/Commands/General/GenerateCommandCommand.php
@@ -20,7 +20,6 @@ class GenerateCommandCommand extends SyntraCommand
             ->setName('general:generate-command')
             ->setDescription('Generates a scaffold for a new Symfony Console command')
             ->addOption('group', null, InputOption::VALUE_OPTIONAL, 'The command group')
-            ->addOption('name', null, InputOption::VALUE_OPTIONAL, 'The PHP class name of the command')
             ->addOption('cli-name', null, InputOption::VALUE_OPTIONAL, 'The CLI name of the command (e.g. refactor:custom)')
             ->addOption('desc', null, InputOption::VALUE_OPTIONAL, 'Description of the command');
     }
@@ -38,13 +37,6 @@ class GenerateCommandCommand extends SyntraCommand
         }
         $group = strtolower($group);
 
-        $name = $this->input->getOption('name')
-            ?? $helper->ask($this->input, $this->output, new Question('PHP class name (e.g. MyCoolCommand): '));
-        if (!$name || !preg_match('/^[A-Z][A-Za-z0-9_]+Command$/', (string) $name)) {
-            $this->output->error('Class name must start with an uppercase letter and end with "Command"');
-            return Command::FAILURE;
-        }
-
         $cliName = $this->input->getOption('cli-name')
             ?? $helper->ask($this->input, $this->output, new Question('CLI name (e.g. my-cool): '));
         if (!$cliName || !preg_match('/^[a-z][a-z0-9\-:]+$/', (string) $cliName)) {
@@ -55,6 +47,7 @@ class GenerateCommandCommand extends SyntraCommand
         $desc = $this->input->getOption('desc')
             ?? $helper->ask($this->input, $this->output, new Question('Description: '));
 
+        $name = str_replace('-', '', ucwords($cliName, '-')) . 'Command';
         $namespace = 'Vix\Syntra\Commands\\' . ucfirst($group);
         $fileDir = PACKAGE_ROOT . '/src/Commands/' . ucfirst($group);
         $filePath = "$fileDir/$name.php";
