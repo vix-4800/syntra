89592118b9965cd9ff9a227bc257989f2dc56275
Anton Vix <Gordan.ei@protonmail.ch>
2025-07-03 01:02:35 +0400

    UserFindOneToIdentityRector full rewrite

--- src/Commands/Rector/UserFindOneToIdentityRector.php
+++ src/Commands/Rector/UserFindOneToIdentityRector.php
@@ -5,16 +5,12 @@ declare(strict_types=1);
 namespace Vix\Syntra\Commands\Rector;
 
 use PhpParser\Node;
+use PhpParser\Node\Expr;
 use PhpParser\Node\Name;
-use PhpParser\Node\Identifier;
-use PhpParser\Node\Expr\Array_;
-use PhpParser\Node\Expr\ArrayItem;
 use Rector\Rector\AbstractRector;
-use PhpParser\Node\Expr\MethodCall;
 use PhpParser\Node\Expr\PropertyFetch;
 use PhpParser\Node\Expr\StaticCall;
 use PhpParser\Node\Expr\StaticPropertyFetch;
-use PhpParser\Node\Scalar\String_;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 
 class UserFindOneToIdentityRector extends AbstractRector
@@ -22,7 +18,7 @@ class UserFindOneToIdentityRector extends AbstractRector
     public function getRuleDefinition(): RuleDefinition
     {
         return new RuleDefinition(
-            'Replaces redundant User::findOne(...) lookups for current user with Yii::$app->user->identity',
+            'Replaces User::findOne(Yii::$app->user->id) with Yii::$app->user->identity',
             []
         );
     }
@@ -32,149 +28,68 @@ class UserFindOneToIdentityRector extends AbstractRector
      */
     public function getNodeTypes(): array
     {
-        return [StaticCall::class, MethodCall::class];
+        return [StaticCall::class];
     }
 
     /**
-     * @param StaticCall|MethodCall $node
+     * @param StaticCall $node
      */
     public function refactor(Node $node): ?Node
     {
-        // User::findOne(...)
-        if (
-            $node instanceof StaticCall
-            && $node->class instanceof Name
-            && $this->isName($node->class, 'User')
-            && $this->isName($node->name, 'findOne')
-            && count($node->args) === 1
-        ) {
-            $arg = $node->args[0]->value;
-
-            // User::findOne(Yii::$app->user->id) и вариации
-            if ($this->isCurrentUserIdExpr($arg)) {
-                return $this->userIdentityExpr();
-            }
-
-            // User::findOne(['id' => ...])
-            if ($arg instanceof Array_) {
-                foreach ($arg->items as $item) {
-                    if (
-                        $item instanceof ArrayItem
-                        && $item->key instanceof String_
-                        && $item->key->value === 'id'
-                        && $this->isCurrentUserIdExpr($item->value)
-                    ) {
-                        return $this->userIdentityExpr();
-                    }
-                }
-            }
+        if (!$this->isUserFindOneCall($node)) {
+            return null;
         }
 
-        if (
-            $node instanceof MethodCall
-            && $this->isName($node->name, 'one')
-            && $node->var instanceof MethodCall
-            && $this->isName($node->var->name, 'where')
-            && $node->var->var instanceof StaticCall
-            && $node->var->var->class instanceof Name
-            && $this->isName($node->var->var->class, 'User')
-            && $this->isName($node->var->var->name, 'find')
-            && isset($node->var->args[0])
-        ) {
-            $whereArg = $node->var->args[0]->value;
+        $args = $node->getArgs();
+        if (count($args) !== 1) {
+            return null;
+        }
 
-            if ($whereArg instanceof Array_) {
-                foreach ($whereArg->items as $item) {
-                    if (
-                        $item instanceof ArrayItem
-                        && $item->key instanceof String_
-                        && $item->key->value === 'id'
-                        && $this->isCurrentUserIdExpr($item->value)
-                    ) {
-                        return $this->userIdentityExpr();
-                    }
-                }
-            }
+        $argValue = $args[0]->value;
+
+        if ($this->isYiiUserIdPropertyFetch($args[0]->value)) {
+            return $this->createYiiUserIdentityPropertyFetch();
         }
 
         return null;
     }
 
-    /**
-     * Check if expression is any variant of current user id
-     */
-    private function isCurrentUserIdExpr(Node $expr): bool
+    private function isUserFindOneCall(StaticCall $node): bool
     {
-        // Yii::$app->user->id
-        if (
-            $expr instanceof PropertyFetch
-            && $expr->name instanceof Identifier && $expr->name->toString() === 'id'
-            && $expr->var instanceof PropertyFetch
-            && $expr->var->name instanceof Identifier && $expr->var->name->toString() === 'user'
-            && $expr->var->var instanceof StaticPropertyFetch
-            && $expr->var->var->class instanceof Name && $this->isName($expr->var->var->class, 'Yii')
-            && $expr->var->var->name instanceof Identifier && $expr->var->var->name->toString() === 'app'
-        ) {
-            return true;
+        // Проверяем что метод называется findOne
+        if (!$this->isName($node->name, 'findOne')) {
+            return false;
         }
 
-        // Yii::$app->user->getId()
-        if (
-            $expr instanceof MethodCall
-            && $expr->name instanceof Identifier && $expr->name->toString() === 'getId'
-            && $expr->var instanceof PropertyFetch
-            && $expr->var->name instanceof Identifier && $expr->var->name->toString() === 'user'
-            && $expr->var->var instanceof StaticPropertyFetch
-            && $expr->var->var->class instanceof Name && $this->isName($expr->var->var->class, 'Yii')
-            && $expr->var->var->name instanceof Identifier && $expr->var->var->name->toString() === 'app'
-        ) {
-            return true;
-        }
-
-        // Yii::$app->user->identity->getId()
-        if (
-            $expr instanceof MethodCall
-            && $expr->name instanceof Identifier && $expr->name->toString() === 'getId'
-            && $expr->var instanceof PropertyFetch
-            && $expr->var->name instanceof Identifier && $expr->var->name->toString() === 'identity'
-            && $expr->var->var instanceof PropertyFetch
-            && $expr->var->var->name instanceof Identifier && $expr->var->var->name->toString() === 'user'
-            && $expr->var->var->var instanceof StaticPropertyFetch
-            && $expr->var->var->var->class instanceof Name && $this->isName($expr->var->var->var->class, 'Yii')
-            && $expr->var->var->var->name instanceof Identifier && $expr->var->var->var->name->toString() === 'app'
-        ) {
-            return true;
-        }
-
-        // Yii::$app->user->getIdentity()->getId()
-        if (
-            $expr instanceof MethodCall
-            && $expr->name instanceof Identifier && $expr->name->toString() === 'getId'
-            && $expr->var instanceof MethodCall
-            && $expr->var->name instanceof Identifier && $expr->var->name->toString() === 'getIdentity'
-            && $expr->var->var instanceof PropertyFetch
-            && $expr->var->var->name instanceof Identifier && $expr->var->var->name->toString() === 'user'
-            && $expr->var->var->var instanceof StaticPropertyFetch
-            && $expr->var->var->var->class instanceof Name && $this->isName($expr->var->var->var->class, 'Yii')
-            && $expr->var->var->var->name instanceof Identifier && $expr->var->var->var->name->toString() === 'app'
-        ) {
-            return true;
+        // Проверяем что класс - User (базовое имя без неймспейса)
+        if ($node->class instanceof Name) {
+            $className = $this->getName($node->class);
+            $baseName = $className ? basename(str_replace('\\', '/', $className)) : null;
+            return $baseName === 'User';
         }
 
         return false;
     }
 
-    /**
-     * Returns node Yii::$app->user->identity
-     */
-    private function userIdentityExpr(): Node
+    private function isYiiUserIdPropertyFetch(Expr $expr): bool
+    {
+        return $expr instanceof PropertyFetch
+            && $this->getName($expr->name) === 'id'
+            && $expr->var instanceof PropertyFetch
+            && $this->getName($expr->var->name) === 'user'
+            && $expr->var->var instanceof StaticPropertyFetch
+            && $this->isName($expr->var->var->class, 'Yii')
+            && $this->isName($expr->var->var->name, 'app');
+    }
+
+    private function createYiiUserIdentityPropertyFetch(): PropertyFetch
     {
         return new PropertyFetch(
             new PropertyFetch(
-                new StaticPropertyFetch(new Name('Yii'), new Identifier('app')),
-                new Identifier('user')
+                new StaticPropertyFetch(new Name('Yii'), 'app'),
+                'user'
             ),
-            new Identifier('identity')
+            'identity'
         );
     }
 }
