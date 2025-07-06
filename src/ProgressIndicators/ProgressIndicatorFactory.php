67c02b61715210cf2dd0b55b23fb8074ddfe63d7
Anton <Gordan.ei@protonmail.ch>
2025-07-06 20:24:33 +0400

    docs: document --no-progress option

--- src/ProgressIndicators/ProgressIndicatorFactory.php
+++ src/ProgressIndicators/ProgressIndicatorFactory.php
@@ -6,11 +6,13 @@ namespace Vix\Syntra\ProgressIndicators;
 
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Vix\Syntra\ProgressIndicators\ProgressIndicatorInterface;
+use Vix\Syntra\ProgressIndicators\NullProgressIndicator;
 
 class ProgressIndicatorFactory
 {
     public const TYPE_SPINNER = 'spinner';
     public const TYPE_PROGRESS_BAR = 'progress_bar';
+    public const TYPE_NONE = 'none';
 
     public static function create(
         string $type,
@@ -20,6 +22,7 @@ class ProgressIndicatorFactory
         return match ($type) {
             self::TYPE_PROGRESS_BAR => new ProgressBarIndicator($output, $maxSteps),
             self::TYPE_SPINNER => new SpinnerIndicator($output),
+            self::TYPE_NONE => new NullProgressIndicator(),
             default => new SpinnerIndicator($output),
         };
     }
