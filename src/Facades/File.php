5e0c5f3ecd666117415d37d03293fa6098171b4a
Anton Vix <Gordan.ei@protonmail.ch>
2025-07-06 23:00:06 +0400

    File facade update

--- src/Facades/File.php
+++ src/Facades/File.php
@@ -7,7 +7,11 @@ namespace Vix\Syntra\Facades;
 use Vix\Syntra\Utils\FileHelper;
 
 /**
- * @method static array collectFiles(string $dir, array $extensions = ['php'], bool $absolute = true)
+ * @method static array  collectFiles(string $dir, array $extensions = ['php'], bool $absolute = true)
+ * @method static void   writeChanges(string $filePath, string $oldContent, string $newContent)
+ * @method static string makeRelative(string $path, string $root)
+ * @method static void   setCacheEnabled(bool $enabled)
+ * @method static void   clearCache()
  */
 class File extends Facade
 {
