a018ee13a9a6b7c114be4143ce5993d3506d1a7b
Anton <Gordan.ei@protonmail.ch>
2025-07-07 00:28:46 +0400

    Warn for high danger level commands

--- src/Commands/SyntraRefactorCommand.php
+++ src/Commands/SyntraRefactorCommand.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Vix\Syntra\Commands;
 
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Vix\Syntra\Enums\DangerLevel;
 use Vix\Syntra\Commands\SyntraCommand;
 use Vix\Syntra\Traits\HasDangerLevel;
 
@@ -25,5 +27,21 @@ abstract class SyntraRefactorCommand extends SyntraCommand
         $this->force = (bool) $input->getOption('force');
 
         parent::initialize($input, $output);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        if ($this->getDangerLevel() !== DangerLevel::LOW) {
+            $output->writeln(sprintf(
+                '<comment>Warning: this command is marked as %s and may be unsafe.</comment>',
+                $this->getDangerLevel()->value,
+            ));
+        }
+
+        if (!$this->askDangerConfirmation($input, $output)) {
+            return Command::FAILURE;
+        }
+
+        return parent::execute($input, $output);
     }
 }
