9505b1cc3085063928dd74751ab2b2125badfd9e
Anton <Gordan.ei@protonmail.ch>
2025-07-07 17:21:17 +0400

    Inject rector executor via container (#56)
    * refactor: inject RectorCommandExecutor
    
    * RectorRunnerCommand __construct refactor

--- src/Commands/RectorRunnerCommand.php
+++ src/Commands/RectorRunnerCommand.php
@@ -12,12 +12,9 @@ use Vix\Syntra\Utils\RectorCommandExecutor;
  */
 abstract class RectorRunnerCommand extends SyntraRefactorCommand
 {
-    protected RectorCommandExecutor $rectorExecutor;
-
-    public function __construct()
+    public function __construct(protected RectorCommandExecutor $rectorExecutor)
     {
         parent::__construct();
-        $this->rectorExecutor = new RectorCommandExecutor();
     }
 
     /**
