ba1c5cff7319ec09c93bcab72e9ddc34cfc55fcb
Anton <Gordan.ei@protonmail.ch>
2025-07-09 13:56:08 +0400

    Refactor GenerateDocsCommand

--- tests/Commands/GenerateDocsCommandTest.php
+++ tests/Commands/GenerateDocsCommandTest.php
@@ -29,4 +29,28 @@ class GenerateDocsCommandTest extends CommandTestCase
         $this->assertMatchesRegularExpression('/\| `view`\s+\|\s+0\s+\|\s+\$slug\s+\|\s+\|/', $content);
 
     }
+
+    public function testReferenceCountsIncluded(): void
+    {
+        mkdir("$this->dir/backend/controllers", 0777, true);
+        copy(
+            __DIR__ . '/../Fixtures/backend/controllers/SiteController.php',
+            "$this->dir/backend/controllers/SiteController.php"
+        );
+        mkdir("$this->dir/backend/views/site", 0777, true);
+        file_put_contents("$this->dir/backend/views/site/index.phtml", '/site/index /site/index');
+        file_put_contents(
+            "$this->dir/composer.json",
+            json_encode(['require' => ['yiisoft/yii2' => '*']])
+        );
+
+        $this->runCommand('general:generate-docs', [
+            'path' => $this->dir,
+            '--no-progress' => true,
+            '--count-refs' => true,
+        ]);
+
+        $content = file_get_contents("$this->dir/docs/routes.md");
+        $this->assertMatchesRegularExpression('/\| `index`\s+\|\s+2\s+\|/', $content);
+    }
 }
